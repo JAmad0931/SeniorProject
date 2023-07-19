4c448d716f590372d37d1245144681301dac2d78
JAmad0931 <Josueamador727@yahoo.com>
2023-07-19 18:28:27 -0500

    new items

--- javascript/dropDown.js
+++ javascript/dropDown.js
@@ -2,7 +2,7 @@ function dropDownFunc() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
   
-  // Close the dropdown menu if the user clicks outside of it
+  // Close the dropdown account menu if the user clicks outside of it
   window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
       var dropdowns = document.getElementsByClassName("dropdown-content");
@@ -15,3 +15,21 @@ function dropDownFunc() {
       }
     }
   }
+// Function for drop down menu of navbar links
+function burgerDropdown() {
+  document.getElementById("burgerDropdown").classList.toggle("show");
+}
+
+// Close the dropdown burger menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("burgerDropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
