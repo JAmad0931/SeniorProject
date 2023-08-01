4bd5be2d90f81855c712e8d36368e83466b3ba0d
Josue Amador <60362964+JAmad0931@users.noreply.github.com>
2023-07-31 19:08:44 -0500

    Add files via upload

---
+++ public/javascript/ui.js
@@ -0,0 +1,8 @@
+
+export const txtEmail = document.querySelector('#email');
+export const txtPassword = document.querySelector('#pwd');
+
+export const btnLogin = document.getElementById('btnLogin');
+export const btnSignup = document.querySelector('#btnSignup');
+
+export const btnLogout = document.querySelector('#btnlogout');
